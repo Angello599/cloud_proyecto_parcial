5f7ebfb1dc4072b1ca64e0a9c1bd2f92de70d87e
Angello599 <62721240+Angello599@users.noreply.github.com>
2024-05-20 00:07:36 -0500

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    libros = requests.get('http://lb-prod-proyecto-498384887.us-east-1.elb.amazonaws.com:8000/libros').json()
+    usuarios = requests.get('http://lb-prod-proyecto-498384887.us-east-1.elb.amazonaws.com:8001/usuarios').json()
+    prestamos = requests.get('http://lb-prod-proyecto-498384887.us-east-1.elb.amazonaws.com:8002/prestamos').json()
+    return render_template('index.html', libros=libros['libros'], usuarios=usuarios['usuarios'], prestamos=prestamos['prestamos'])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8003)
